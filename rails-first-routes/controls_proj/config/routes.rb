d9285e615105349d0d1404106bc9b03dbd9528a6
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-03 15:19:44 -0700

    pt2

--- rails-first-routes/controls_proj/config/routes.rb
+++ rails-first-routes/controls_proj/config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
 
 
   #VERB, PATH,          CONTROLLER#ACTION,  PREFIX's
-  get 'users/:id', to: 'users#index', as: 'users'
-  post 'users/:id', to: 'users#create'
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
   get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user' 
-
-
-
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 
 
   # Defines the root path route ("/")
