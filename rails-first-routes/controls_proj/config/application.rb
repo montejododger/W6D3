1477cac04db92544be1545ef38a31a2ca84b56e9
Matthew Montejo <montejododger@yahoo.com>
2023-05-03 14:43:01 -0700

    projected started and routes being established

--- rails-first-routes/controls_proj/config/application.rb
+++ rails-first-routes/controls_proj/config/application.rb
@@ -22,6 +22,7 @@ module ControlsProj
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # skip_before_action :verify_authenticity_token
 
     # Configuration for the application, engines, and railties goes here.
     #
--- rails-first-routes/controls_proj/config/routes.rb
+++ rails-first-routes/controls_proj/config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # resources :users
+  # resources :users, only:[:create, :destroy] -- When you "Only" want to create the routes in the arr
+  # resources :users, except:[:create, :destroy] -- When you  wnat to create everything "Except" routes in the arr
+
+
+  #VERB, PATH,          CONTROLLER#ACTION,  PREFIX's
+  get 'users/:id', to: 'users#index', as: 'users'
+  post 'users/:id', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user' 
+
+
+
+
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
