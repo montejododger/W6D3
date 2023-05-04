5b6ede84fb4e3b2dee98d0e1827dbe56923cb20b
Matthew Montejo <montejododger@yahoo.com>
2023-05-03 17:28:08 -0700

    pt4 missing update & destroy

--- rails-first-routes/controls_proj/app/controllers/users_controller.rb
+++ rails-first-routes/controls_proj/app/controllers/users_controller.rb
@@ -8,13 +8,19 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(params.require(:user).permit(:name, :email))
-        @user.save!
-        render json: @user
+
+        if @user.save
+            render json: @user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+
+            
     end
 
     def show
-        
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
 
 
